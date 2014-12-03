0eec0393625f819629de6f154ee99ca22e8f4d79
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:33:38 +0100

    Pasamos como parametro el nombre  de nuestros hilos.

--- repaso4/Hilo.java
+++ repaso4/Hilo.java
@@ -5,11 +5,40 @@
  */
 package repaso4;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author findag
  */
-public class Hilo
+public class Hilo extends Thread
 {
     
+    public Hilo(String nombre)
+    {
+        super(nombre);
+    }
+    
+    @Override
+    public void run()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println("Esta es la iteracion " + i + " del " + getName());
+            
+            try
+            {
+                // Dormimos el proceso para poder ver como mas facilidad si respeta la espera.
+                sleep(100);
+                
+                //Usamos yield() para dejar paso a la iteracion del otro hilo.
+                yield();
+            }
+            catch (InterruptedException ex)
+            {
+                System.out.println("Error " + ex);
+            }
+        }
+    }
 }
