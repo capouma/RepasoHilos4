0b639dd05bb56ddc3fc7d5bd453b73369b5d4475
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:20:44 +0100

    Iniciamos el ejercicio 4 de repaso

---
+++ repaso4/Repaso4.java
@@ -0,0 +1,21 @@
+/*
+ * Programa en java que crea dos hilos y los ejecuta. Los hilos escriben 5 veces 
+ * el número de iteración del bucle y su nombre. En cada iteración, después de escribir su nombre,
+ * dejan paso al otro hilo (se bloquean utilizando el método yield()).
+ */
+package repaso4;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
